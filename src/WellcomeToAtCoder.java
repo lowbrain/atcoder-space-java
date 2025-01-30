0c256a2f7e475bc2bca9a25889d6866a7eb5fd37
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-01-30 13:05:31 +0000

    A - Welcome to AtCoder

---
+++ src/WellcomeToAtCoder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WellcomeToAtCoder {
+
+    public static void main(String[] args) {
+        String[] lines = new String[3];
+        int sum = 0;
+
+        // 入力
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < lines.length; i++) lines[i] = scan.nextLine();
+
+        // 1行目
+        sum += Integer.parseInt(lines[0].trim());
+
+        // 2行目
+        for (String str : lines[1].trim().split(" ")) sum += Integer.parseInt(str);
+
+        // 3行目
+        String str = lines[2];
+
+        // 出力
+        System.out.println(sum + " " + str);
+    }
+}
