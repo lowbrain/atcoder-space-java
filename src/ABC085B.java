0b6a83940c804004848112a3e83df32e83dd1e57
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-02-02 05:20:48 +0000

    ABC085B

---
+++ src/ABC085B.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ABC085B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inN = Integer.valueOf(scanner.nextLine());
+        
+        int[] inDs = new int[inN];
+        for(int i = 0; i < inDs.length; i++) inDs[i] = Integer.valueOf(scanner.nextLine());
+        Arrays.sort(inDs);
+        
+        scanner.close();
+
+        int max = 101;
+        int record = 0;
+        for(int i = inDs.length - 1; i >= 0; i--) {
+            if (max > inDs[i]) {
+                max = inDs[i];
+                record++;
+            }
+        }
+
+        System.out.println(record);
+    }
+}
